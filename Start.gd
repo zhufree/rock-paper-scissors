8f6f6035fc46da0359ae33e4bfef501e51814bf3
roam1n <203mine@gmail.com>
2024-03-05 20:51:43 +0800

    fix:start scene
    - remove sceneManager
    - fix README

--- Start.gd
+++ Start.gd
@@ -1,25 +1,8 @@
 extends Node2D
 
-@onready var game_win_mode_label = %GameWinModeLabel
-@onready var one_button = $ColorRect/VBoxContainer/HBoxContainer/OneButton
-@onready var two_button = $ColorRect/VBoxContainer/HBoxContainer/TwoButton
-@onready var three_button = $ColorRect/VBoxContainer/HBoxContainer/ThreeButton
-@onready var panel_container = %PanelContainer
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	game_win_mode_label.hide()
+	pass
 
-func _on_button_pressed():
-	panel_container.show()
-
-func _on_one_button_pressed():
-	game_win_mode_label.show()
-	game_win_mode_label.text = one_button.text
-
-func _on_two_button_pressed():
-	game_win_mode_label.show()
-	game_win_mode_label.text = two_button.text
-
-func _on_three_button_pressed():
-	game_win_mode_label.show()
-	game_win_mode_label.text = three_button.text
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://Main.tscn")
